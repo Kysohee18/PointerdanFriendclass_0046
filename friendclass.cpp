6094a779f5382317fd862d421c825ab893ca3a13
Kyosohee18 <hanashahky@gmail.com>
2025-05-05 10:51:31 +0700

    int main

--- friendclass.cpp
+++ friendclass.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa{
+class Mahasiswa{
     public:
     int nim;
     void showNim(){
         cout<<"No Induk = " <<nim<<endl;
     }
 };
+
+
+    int main() {
+    Mahasiswa mhs[1];         // array of 1 object of Mahasiswa
+    mhs[0].showNim();         // use index to access array element
+
+  
+
 

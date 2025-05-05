be002d18999156f20c76b464ebb9285fa5990d2a
Kyosohee18 <hanashahky@gmail.com>
2025-05-05 10:52:34 +0700

    friend class

--- friendclass.cpp
+++ friendclass.cpp
@@ -14,6 +14,15 @@ class Mahasiswa{
     Mahasiswa mhs[1];         // array of 1 object of Mahasiswa
     mhs[0].showNim();         // use index to access array element
 
-  
+    Mahasiswa& refmhs = mhs[0];
+    refmhs.nim = 2;
+    mhs[0].showNim();
+
+    Mahasiswa* pmhs = &mhs[0];
+    pmhs->nim = 3;
+    pmhs->showNim();
+
+    return 0;
+}
 
 

85f951b77c9963f51ed80a5ec5f28487dad3048d
Kyosohee18 <hanashahky@gmail.com>
2025-05-05 11:31:50 +0700

    class publis friend dua

---
+++ friendfunctiondua.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class pelajar;
+class manusia
+{
+public:
+    void showNilaipelajar(pelajar & a);
+    //void shownilai peakar2(pelajar &x)
+};
+
+class pelajar
+{
+    private:
+    
+}
